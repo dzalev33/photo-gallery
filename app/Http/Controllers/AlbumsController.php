8fb8329ad625f2e8e865230e334241d3a34aadac
dzalev33 <dzalev33@gmail.com>
2018-06-04 23:50:18 +0200

    delete added and final fixes

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Album;
 use Illuminate\Http\Request;
 
 class AlbumsController extends Controller
 {
    public function index()
    {
-       return view('albums.index');
+       $albums = Album::with('Photos')->get();
+       return view('albums.index')->with('albums', $albums);
    }
 
    public function create()
@@ -32,13 +34,29 @@ class AlbumsController extends Controller
         $extension = $request->file('cover_image')->getClientOriginalExtension();
 
         //create new filename
-       $filenametoStore = $filename. '_'.time().'_'.$extension;
+       $filenameToStore = $filename. '_'.time().'_'.$extension;
 
        //upload image
 
-       $path = $request->file('cover_image')->storeAs('public/album_covers', $filenametoStore);
+       $path = $request->file('cover_image')->storeAs('public/album_covers', $filenameToStore);
 
-       return $path;
+       //create album
 
+       $album = new Album;
+       $album->name = $request->input('name');
+       $album->description = $request->input('description');
+       $album->cover_image = $filenameToStore;
+
+       $album->save();
+
+       return redirect('/albums')->with('success', 'Album Created');
+
+   }
+
+   public function show($id)
+   {
+        $album = Album::with('Photos')->find($id);
+
+        return view('albums.show')->with('album', $album);
    }
 }
--- resources/views/inc/topbar.blade.php
+++ resources/views/inc/topbar.blade.php
@@ -2,7 +2,7 @@
 <div class="row">
     <div class="top-bar-left">
         <ul class="menu">
-            <li class="menu-text">Photoshow</li>
+            <li  class="menu-text">Photoshow</li>
             <li><a href="/">Home</a></li>
             <li><a href="/albums/create">Create Album</a></li>
         </ul>
